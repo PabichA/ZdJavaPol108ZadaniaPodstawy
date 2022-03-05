df97cd9310262ccddecef05bc85360be96967500
AdamP <adampabich@gmail.com>
2022-03-05 10:53:27 +0100

    day1_day1 solution

---
+++ src/pl/sdaacademy/java/basic/exercises/day1/Task1.java
@@ -0,0 +1,18 @@
+package pl.sdaacademy.java.basic.exercises.day1;
+
+class Task1 {
+
+    public static void main(String[] args) {
+        displayWordUsingDecimalFormat();
+        System.out.println();
+    }
+
+    public static void displayWordUsingDecimalFormat() {
+       char S = 83;
+       char d = 68;
+       char a = 65;
+        System.out.print(S);
+        System.out.print(d);
+        System.out.print(a);
+    }
+}
